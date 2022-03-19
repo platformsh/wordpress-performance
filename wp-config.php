9ee642f3d55553f3e882ae52cfcaeb931e150ad8
Jérôme Vieilledent <jerome.vieilledent@blackfire.io>
2022-03-19 20:02:54 +0100

    Add ddev

--- wp-config.php
+++ wp-config.php
@@ -125,5 +125,11 @@ if ( ! defined( 'ABSPATH' ) ) {
   define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
 
+// Include for ddev-managed settings in wp-config-ddev.php.
+$ddev_settings = dirname(__FILE__) . '/wp-config-ddev.php';
+if (is_readable($ddev_settings) && !defined('DB_USER')) {
+    require_once($ddev_settings);
+}
+
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
